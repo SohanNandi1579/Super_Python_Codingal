ce15338f6ec02a923061b537bab7135a0c20884f
SohanNandi1579 <mastersohannandi@gmail.com>
2025-11-16 08:55:44 +0100

    Commit this quickly! And fast!

---
+++ M3/Lesson13/Project.py
@@ -0,0 +1,9 @@
+class AnimalDog:
+    def __init__(self, breed, colour):
+        self.breed = breed
+        self.colour = colour
+
+Doggy1 = AnimalDog("poodle", "red")
+Doggy2 = AnimalDog("mongrel", "all mixed")
+print(Doggy1.breed)
+print(Doggy2.colour)
