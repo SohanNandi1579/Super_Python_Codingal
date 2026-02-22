22c6468e3c018304852d002e50e5431edac6356d
SohanNandi1579 <mastersohannandi@gmail.com>
2026-02-22 09:55:32 +0100

    Commit this quickly! And fast!

--- Module5-TKinter/Demo.py
+++ Module5-TKinter/Demo.py
@@ -1,9 +1,26 @@
+# from tkinter import *
+#
+#
+# window = Tk()
+#
+# window.title("Demo Window")
+# window.geometry('400x300')
+#
+# window.mainloop()
+
 from tkinter import *
+from datetime import date
 
+root = Tk()
+root.title("getting started with Widgets")
+root.geometry('400x300')
 
-window = Tk()
+lbl = Label(text = "Hey There!", fg="white", bg="#072F5F", height = 1, width = 300)
+name_lbl = Label(text="Full Name", bg = "#3895D3")
+name_entry = Entry()
 
-window.title("Demo Window")
-window.geometry('400x300')
+def display():
+    name = name_entry.get()
+    global message
+    message = "Welcome to the Application"
 
-window.mainloop()
